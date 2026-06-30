d350ef8243024fe5fef5c2092708eeb518c42a0b
Akhila Ganireddy <akhigani5@gmail.com>
2026-06-30 21:12:11 +0530

    Implement containsDuplicate method using sorting

--- 217. Contains Duplicate/Solution.java
+++ 217. Contains Duplicate/Solution.java
@@ -1,1 +1,17 @@
+ import java.util.Arrays;
 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int left=0;
+        int right=1;
+        while(right<nums.length){
+            if(nums[left]==nums[right]){
+                return true;
+            }
+            left++;
+            right++;
+        }
+        return false;
+    }
+}
