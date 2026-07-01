85d85a24a84288a2726c270f71df7b9edfee0843
Akhila Gannireddy <akhigani5@gmail.com>
2026-07-01 16:57:04 +0530

    Implement MoveZeroes method to rearrange array

--- 283. Move Zeroes/Solution.java
+++ 283. Move Zeroes/Solution.java
@@ -1,1 +1,13 @@
-
+class solution{
+  public void MoveZeroes(int[] nums){
+    int left=0;
+    for(int right=0;right<nums.length;right++){
+      if(nums[right]!=0){
+        int temp=nums[right];
+        nums[right]=nums[left];
+        nums[left]=temp;
+        left++;
+      }
+    }
+  }
+}
