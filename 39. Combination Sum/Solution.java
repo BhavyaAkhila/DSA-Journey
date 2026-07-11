7bc2c53fd9c3a7737507ddf661e88295db980691
Akhila Gannireddy <akhigani5@gmail.com>
2026-07-11 22:42:55 +0530

    Fix formatting issue in Solution.java

--- 39. Combination Sum/Solution.java
+++ 39. Combination Sum/Solution.java
@@ -20,5 +20,5 @@ class Solution {
         makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
         comb.remove(comb.size() - 1);
         makeCombination(candidates, target, idx + 1, comb, total, res);
-    }    
+    } .   
 }
