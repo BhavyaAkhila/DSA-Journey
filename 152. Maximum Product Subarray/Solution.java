cab582fe2d64a87380d9f53810c5c93a03f3755d
Akhila Gannireddy <akhigani5@gmail.com>
2026-07-15 19:16:00 +0530

    Implement maxProduct method for maximum product subarray

--- 152. Maximum Product Subarray/Solution.java
+++ 152. Maximum Product Subarray/Solution.java
@@ -1,1 +1,24 @@
+class Solution {
+    public int maxProduct(int[] nums) {
 
+        int currentmax = nums[0];
+        int currentmin = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] < 0) {
+                int temp = currentmax;
+                currentmax = currentmin;
+                currentmin = temp;
+            }
+
+            currentmax = Math.max(nums[i], currentmax * nums[i]);
+            currentmin = Math.min(nums[i], currentmin * nums[i]);
+
+            ans = Math.max(ans, currentmax);
+        }
+
+        return ans;
+    }
+}
