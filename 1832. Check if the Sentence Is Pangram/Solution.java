8d78b32dd472306b60de4920bad8ccfc2319a75b
Akhila Gannireddy <akhigani5@gmail.com>
2026-07-17 17:42:53 +0530

    Implement pangram check in Solution class

--- 1832. Check if the Sentence Is Pangram/Solution.java
+++ 1832. Check if the Sentence Is Pangram/Solution.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length()<26){
+            return false;
+        }
+        for(int ch='a';ch<='z';ch++){
+            if(sentence.indexOf(ch)<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
