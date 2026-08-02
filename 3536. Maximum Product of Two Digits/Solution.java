480bdc666561c7a3cfad97e2c965d60358dd250b
Akhila Gannireddy <akhigani5@gmail.com>
2026-08-02 18:27:43 +0530

    Implement maxProduct method to find max digit product

--- 3536. Maximum Product of Two Digits/Solution.java
+++ 3536. Maximum Product of Two Digits/Solution.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int maxProduct(int n) {
+         int a=Integer.MIN_VALUE;
+        int b=Integer.MIN_VALUE;
+        while(n!=0){
+            int temp=n%10;
+            if(temp>=a){ 
+                b=a;
+                a=temp;
+                }
+            else if(temp>b) b=temp;
+            n=n/10;
+        }
+        return a*b;
+    }
+}
