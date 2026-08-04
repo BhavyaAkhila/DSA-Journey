c6bb5d66ba1f2202db1554a8ec93ca0caa843e53
Akhila Gannireddy <akhigani5@gmail.com>
2026-08-04 06:47:13 +0530

    Add findMissingElements method in Solution class
    Implement method to find missing elements in a range.

--- 3731. Find Missing Elements/Solution.java
+++ 3731. Find Missing Elements/Solution.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        boolean[] memo=new boolean[101];
+        for(int num:nums){
+            memo[num]=true;
+            min=Math.min(min,num);
+            max=Math.max(max,num);
+        }
+        List<Integer>res=new ArrayList();
+        for(int i=min;i<=max;i++){
+     if(memo[i]==false)res.add(i);
+        }
+        return res;
+    }
+}
