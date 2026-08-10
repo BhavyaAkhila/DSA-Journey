b97098b7fed3ca2f7cd3c41288f42ed3de011aa6
Akhila Gannireddy <akhigani5@gmail.com>
2026-08-10 08:37:43 +0530

    Update Solution.java

--- 1510. Stone Game IV/Solution.java
+++ 1510. Stone Game IV/Solution.java
@@ -1,1 +1,15 @@
-
+class solution{
+  public boolean winnerSquareGame (int n){
+    boolean[] dp=new boolean[n+1];
+    for(int i=1; i<=n;i++){
+      for(int j=1;j*j<=i;j++){
+        if(!dp[i-j * j]){
+          fp[i]=true;
+          break;
+        }
+      }
+    }
+    return dp[n];
+  }
+}
+        
