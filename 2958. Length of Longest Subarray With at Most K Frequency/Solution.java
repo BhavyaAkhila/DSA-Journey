10dbca3d05f55722fe8eb5fd2b6a47782ee04d29
Akhila Gannireddy <akhigani5@gmail.com>
2026-08-12 06:25:52 +0530

    Update Solution.java

--- 2958. Length of Longest Subarray With at Most K Frequency/Solution.java
+++ 2958. Length of Longest Subarray With at Most K Frequency/Solution.java
@@ -1,1 +1,28 @@
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
 
+        int start=0,end=0,result=0,count=0;
+
+        while(end<nums.length){
+
+            map.put(nums[end],map.getOrDefault(nums[end],0)+1);    
+
+            if(map.get(nums[end])>k) {   
+
+                while(start<end && nums[start]!=nums[end]){ 
+                    map.put(nums[start],map.get(nums[start++])-1);
+                    count--;
+                }
+                start++;
+                map.put(nums[end],map.get(nums[end])-1); 
+
+            }else{
+                result=Math.max(result,++count); 
+            }
+            end++;
+        }
+
+        return result;
+    }
+}
