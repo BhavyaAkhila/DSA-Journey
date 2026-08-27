1530e25d307055c1f7f939504edfd6774899034c
Akhila Gannireddy <akhigani5@gmail.com>
2026-08-27 15:26:58 +0530

    Correct variable declaration for left index
    Fix syntax error in variable declaration.

--- 125. Valid Palindrome/Solution.java
+++ 125. Valid Palindrome/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left=0;
+        int left=0
         int right=s.length()-1;
         while(left<right){
              while (left < right &&
