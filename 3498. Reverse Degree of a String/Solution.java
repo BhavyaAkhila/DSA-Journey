da92991231734b074cc16617e3933a43d2321c3f
Akhila Gannireddy <akhigani5@gmail.com>
2026-09-20 08:35:23 +0530

    Implement reverseDegree method in Solution class

--- 3498. Reverse Degree of a String/Solution.java
+++ 3498. Reverse Degree of a String/Solution.java
@@ -1,1 +1,18 @@
+class Solution {
 
+    public int reverseDegree(String s) {
+
+        int res = 0;
+
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+
+            int charVal = 26 - (chars[i] - 'a');
+
+            res += charVal * (i + 1);
+        }
+
+        return res;
+    }
+}
